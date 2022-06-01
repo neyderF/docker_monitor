23943aaa7bc4545028d6d2f059d3bd4b2b1bcaac
neyderF <49882090+neyderF@users.noreply.github.com>
2022-05-31 20:03:15 -0500

    base

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="./js/tailwindConf.js"></script>
+    <title>Monitor del sistema</title>
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+
+    <div class="p-6 max-w-sm mx-auto bg-red rounded-xl shadow-lg flex items-center space-x-4">
+        <div class="shrink-0">
+            <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+        </div>
+        <div>
+            <div class="text-xl font-medium text-black">ChitChat</div>
+            <p class="text-slate-500">You have a new message!</p>
+        </div>
+    </div>
+</body>
+
+</html>
