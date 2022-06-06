084aa48a571080f924c5548f3702cfe59874c16e
neyderF <49882090+neyderF@users.noreply.github.com>
2022-06-06 11:56:08 -0500

    gauge

--- index.php
+++ index.php
@@ -6,46 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="./js/tailwindConf.js"></script>
     <link href="./css/all.min.css" rel="stylesheet" title="Default Style">
     <script src="./js/all.min.js"></script>
-    <script>
-        // It's best to inline this in `head` to avoid FOUC (flash of unstyled content) when changing pages or themes
-        if (
-            localStorage.getItem('color-theme') === 'dark' ||
-            (!('color-theme' in localStorage) &&
-                window.matchMedia('(prefers-color-scheme: dark)').matches)
-        ) {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-        }
-    </script>
     <title>Monitor de recursos</title>
 </head>
 
 <body class="bg-zinc-50 dark:bg-slate-800">
-    <header class="mx-auto flex flex-row p-3 items-center bg-red-500 dark:bg-slate-700 shadow-lg shadow-red-500/50 ">
+    <header class="mx-auto flex flex-col  md:flex-row p-3 items-center bg-red-500 dark:bg-slate-700 shadow-lg shadow-red-500/50 ">
         <div class="flex grow">
             <h1 class="text-3xl font-bold  text-white dark:text-slate-200">
                 <!-- <i class="fa fa-desktop"></i> -->
                 Monitor de recursos
             </h1>
         </div>
-        <nav class="flex mx-5 ">
+        <nav class="flex mx-5 mt-5 md:mt-0 ">
 
             <a class="mr-5 text-white hover:opacity-80" href="#info-system"><i class="fa-solid fa-server"></i> Información del sistema</a>
 
             <a class="text-white hover:opacity-80" href="#info-dockers"><i class="fa-brands fa-docker"></i> Dockers</a>
         </nav>
-        <button id="theme-toggle" type="button" class="text-slate-700 w-10 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-            <svg id="theme-toggle-dark-icon" class="w-5 h-5 hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-            </svg>
-            <svg id="theme-toggle-light-icon" class="w-5 h-5 hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
-            </svg>
-        </button>
 
     </header>
     <div class="bg-red-500 h-[74px]">
@@ -66,7 +47,7 @@
                 Recursos del sistema
             </h1>
             <h3 class="text-md font-normal max-w-sm  text-center text-zinc-800 dark:text-slate-200 mt-2">
-                Informe estadistico acerca del estado de la memoria ram, CPU, y uso del disco del servidor anfitrion
+                Informe estadistico acerca del porcentaje de uso de la memoria ram, CPU, y disco del servidor anfitrión
             </h3>
         </div>
         <div class="flex justify-evenly mt-10 flex-wrap">
@@ -75,34 +56,39 @@
                 <div class="animate-pulse rounded-md bg-slate-200 h-44 w-full">
                 </div>
 
+
                 <div class="flex-1 space-y-2 py-1 mt-3">
                     <h3 class="font-bold text-red-500"> <i class="fa fa-hard-drive"></i> Uso del disco</h3>
-                    <div class="mt-1">
+                    <!-- <div class="mt-1">
                         <p class="text-sm text-zinc-900">Esta grafica presenta...</p>
-                    </div>
-                  
+                    </div> -->
+
                 </div>
             </div>
-            <div class="border  bg-white shadow-lg shadow-zinc-300/50 rounded-md p-4 max-w-md w-full mx-2 flex flex-col my-2">
-                <div class="animate-pulse rounded-md bg-slate-200 h-44 w-full">
+            <div class="border bg-white shadow-lg shadow-zinc-300/50 rounded-md p-4 max-w-md w-full mx-2 flex flex-col my-2">
+
+                <div class="flex items-center justify-center rounded-md bg-slate-200 h-44 p-5 ">
+                    <div id="chart_cpu_div" style=" height: 120px;"></div>
                 </div>
 
-                <div class="flex-1 space-y-2 py-1 mt-3">
+                <div class="flex flex-col py-1 text-center  mt-3">
                     <h3 class="font-bold text-red-500"><i class="fa fa-microchip"></i> Uso de cpu</h3>
-                    <div class="mt-1">
-                        <p class="text-sm text-zinc-900">Esta grafica presenta...</p>
-                    </div>
+                    <!-- <div class="mt-1">
+                        <p class="text-sm text-zinc-900">Esta grafica presenta el uso de CPU</p>
+                    </div> -->
                 </div>
             </div>
-            <div class="border  bg-white shadow-lg shadow-zinc-300/50 rounded-md p-4 max-w-md w-full mx-2 flex flex-col my-2">
-                <div class="animate-pulse rounded-md bg-slate-200 h-44 w-full">
+            <div class="border bg-white shadow-lg shadow-zinc-300/50 rounded-md p-4 max-w-md w-full mx-2 flex flex-col my-2">
+
+                <div class="flex items-center justify-center rounded-md bg-slate-200 h-44 p-5 ">
+                    <div id="chart_memory_div" style=" height: 120px;"></div>
                 </div>
 
-                <div class="flex-1 space-y-2 py-1 mt-3">
-                    <h3 class="font-bold text-red-500"><i class="fa fa-memory"></i> Uso de memoria ram</h3>
-                    <div class="mt-1">
-                        <p class="text-sm text-zinc-900">Esta grafica presenta...</p>
-                    </div>
+                <div class="flex flex-col py-1 text-center  mt-3">
+                    <h3 class="font-bold text-red-500"><i class="fa fa-microchip"></i> Uso de memoria RAM</h3>
+                    <!-- <div class="mt-1">
+        <p class="text-sm text-zinc-900">Esta grafica presenta el uso de CPU</p>
+    </div> -->
                 </div>
             </div>
     </section>
@@ -111,14 +97,6 @@
     <!-- Inicia información de los dockers -->
     <section id="info-dockers" class="container mx-auto">
 
-        <button id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-            <svg id="theme-toggle-dark-icon" class="w-5 h-5 hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-            </svg>
-            <svg id="theme-toggle-light-icon" class="w-5 h-5 hidden" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
-            </svg>
-        </button>
     </section>
     <!--Finaliza Información de los dockers -->
     <footer>
@@ -127,7 +105,76 @@
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
-    <script src="./js/main.js"></script>
+    <script>
+        google.charts.load('current', {
+            'packages': ['gauge']
+        });
+        google.charts.setOnLoadCallback(drawCharts);
+
+        /**Función ue renderiza los graficos
+         * 
+         */
+        function drawCharts() {
+
+            drawCPUChart()
+            drawMemoryChart();
+        }
+
+        /**Se renderiza el grafico Gauge para mostrar el uso de la CPU
+         * 
+         */
+        function drawCPUChart() {
+
+            var data = google.visualization.arrayToDataTable([
+                ['Label', 'Value'],
+                ['CPU', 44],
+            ]);
+
+            var options = {
+                width: 400,
+                height: 120,
+                redFrom: 90,
+                redTo: 100,
+                yellowFrom: 75,
+                yellowTo: 90,
+                minorTicks: 5
+            };
+
+
+            var chart = new google.visualization.Gauge(document.getElementById('chart_cpu_div'));
+
+            chart.draw(data, options);
+
+
+        }
+
+
+        /**Se renderiza el grafico Gauge para mostrar el uso de la memoria ram
+         * 
+         */
+        function drawMemoryChart() {
+
+            var data = google.visualization.arrayToDataTable([
+                ['Label', 'Value'],
+                ['RAM', 90],
+            ]);
+
+            var options = {
+                width: 400,
+                height: 120,
+                redFrom: 90,
+                redTo: 100,
+                yellowFrom: 75,
+                yellowTo: 90,
+                minorTicks: 5
+            };
+
+
+            var chart = new google.visualization.Gauge(document.getElementById('chart_memory_div'));
+
+            chart.draw(data, options);
+        }
+    </script>
 </body>
 
 </html>
