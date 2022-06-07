03d02149e7faa56dabb00c7e21f2f44e496796db
neyderF <49882090+neyderF@users.noreply.github.com>
2022-06-07 17:49:25 -0500

    logica para los contenedores

--- monitor.sh
+++ monitor.sh
@@ -7,6 +7,9 @@ cp $INPUT_FILE $OUTPUT_FILE
 #Memoria libre
 MEMORY_FREE=$(free -m | head -2 | tail -n 1 | awk ' {print $4 }')
 
+#Informacion de contenedores en formato JSON
+CONTAINERS_INFO=$(docker stats --no-stream -a --format "{\"containerID\":\"{{ .Container }}\",\"containerName\":\"{{ .Name }}\",\"memory\":{\"raw\":\"{{ .MemUsage }}\",\"percent\":\"{{ .MemPerc }}\"},\"cpu\":\"{{ .CPUPerc }}\"},")
+
 sed -i 's/<%=valor_memoria>/'$MEMORY_FREE'/g' $OUTPUT_FILE
 
 
