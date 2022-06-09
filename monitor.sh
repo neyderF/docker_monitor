8914ef7ddd98ce9c781afeae5e6eb10697e234eb
neyderF <49882090+neyderF@users.noreply.github.com>
2022-06-09 11:07:00 -0500

    ajuste de script de memoria ram

--- monitor.sh
+++ monitor.sh
@@ -18,11 +18,13 @@ USE_DISK=$(df -Bm | head -25 | tail -n 24 | sed '/snap/d' | sed 's/M//g ' | sed 
 
 #Memoria libre
 MEMORY_FREE=$(free -m | head -2 | tail -n 1 | awk ' {print $4 }')
-sed -i 's/<%=valor_memoria>/'$MEMORY_FREE'/g' $OUTPUT_FILE
 
-#Al final no pude hacer lo de los docker reemplazando, entonces toca partir el documento en un unico punto
-#Pero eso se hace al final
-#OJO En el template.html hacer TODO encima del metodo loadDockersInfo
+#Memoria total
+TOTAL_MEMORY=$(free -m | head -2 | tail -n 1 | awk ' {print $2 }')
+
+sed -i 's/<%=valor_memoria>/'$MEMORY_FREE'/g' $OUTPUT_FILE
+sed -i 's/<%=valor_total_memoria>/'$TOTAL_MEMORY'/g' $OUTPUT_FILE
+
 CONTAINERS_INFO=`docker stats --no-stream -a --format "{\"containerID\":\"{{ .Container }}\",\"containerName\":\"{{ .Name }}\",\"memory\":{\"percent\":\"{{ .MemPerc }}\"},\"cpu\":\"{{ .CPUPerc }}\"},"`
 echo "$CONTAINERS_INFO" >> start.html
 
