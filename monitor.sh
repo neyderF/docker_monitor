6e9d7cfc09df4dc9cd426baf02c0d6bab7d3e3a5
neyderF <49882090+neyderF@users.noreply.github.com>
2022-06-09 14:21:56 -0500

    ajuste de template y script

--- monitor.sh
+++ monitor.sh
@@ -8,22 +8,20 @@ cp $INPUT_FILE $OUTPUT_FILE
 NUCLEOS=$(cat /proc/cpuinfo | grep "cpu cores" | cut -d':' -f2)
 
 #Uso del procesador
-PROCESSOR=$(top -bn2 | grep '%Cpu' | tail -1 |awk '{print 100-$8 }')
-USE_PROCESSOR=$((100-($PROCESSOR * $NUCLEOS)))
+PROCESSOR=$(top -bn2 | grep '%Cpu' | tail -1 |awk '{print $8 }' | cut -d',' -f1)
+USE_PROCESSOR=$((100-$PROCESSOR / $NUCLEOS))
 sed -i 's/<%=valor_procesador>/'$USE_PROCESSOR'/g' $OUTPUT_FILE
-
-#Uso de disco por directorios montados, usar el grafico de tabla que se especifica mejor el punto de montaje, procentaje de carga
-USE_DISK=$(df | tail -n+2 | awk {' print "['\''" $6 "'\'',"$3"],"'})
-sed -i 's/<%=valor_disco>/'$USE_DISK'/g' $OUTPUT_FILE
 
 #Memoria libre
 MEMORY_FREE=$(free -m | head -2 | tail -n 1 | awk ' {print $4 }')
-
 #Memoria total
 TOTAL_MEMORY=$(free -m | head -2 | tail -n 1 | awk ' {print $2 }')
-
 sed -i 's/<%=valor_memoria>/'$MEMORY_FREE'/g' $OUTPUT_FILE
 sed -i 's/<%=valor_total_memoria>/'$TOTAL_MEMORY'/g' $OUTPUT_FILE
+
+#Uso de disco por directorios montados, usar el grafico de tabla que se especifica mejor el punto de montaje, procentaje de carga
+USE_DISK=$(df | tail -n+2 | awk {' print "['\''" $6 "'\'',"$3"],"'})
+# sed -i 's/<%=valor_disco>/'$USE_DISK'/g' $OUTPUT_FILE
 
 CONTAINERS_INFO=`docker stats --no-stream -a --format "{\"containerID\":\"{{ .Container }}\",\"containerName\":\"{{ .Name }}\",\"memory\":{\"percent\":\"{{ .MemPerc }}\"},\"cpu\":\"{{ .CPUPerc }}\"},"`
 echo "$CONTAINERS_INFO" >> start.html
