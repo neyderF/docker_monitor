b109d2ac75c3b088651b33756dfb2f92b6721c1b
neyderF <49882090+neyderF@users.noreply.github.com>
2022-06-09 11:58:42 -0500

    ajuste de script

--- monitor.sh
+++ monitor.sh
@@ -8,13 +8,13 @@ cp $INPUT_FILE $OUTPUT_FILE
 NUCLEOS=$(cat /proc/cpuinfo | grep "cpu cores" | cut -d':' -f2)
 
 #Uso del procesador
-PROCESSOR=$(top -n1 | head -3 | tail  -n1  | awk '{print $4}' | cut -d',' -f1)
-USE_PROCESSOR=$(($PROCESSOR * $NUCLEOS))
+PROCESSOR=$(top -bn2 | grep '%Cpu' | tail -1 | grep -P '(....|...) id,'|awk '{print $8 }')
+USE_PROCESSOR=$((100-($PROCESSOR * $NUCLEOS)))
 sed -i 's/<%=valor_procesador>/'$USE_PROCESSOR'/g' $OUTPUT_FILE
 
 #Uso de disco por directorios montados, usar el grafico de tabla que se especifica mejor el punto de montaje, procentaje de carga
-USE_DISK=$(df -Bm | head -25 | tail -n 24 | sed '/snap/d' | sed 's/M//g ' | sed 's/%// ' | awk {' print "['\''"  $6 "'\''"  "," "'\''" "Volumenes" "'\''"  ","$5","$4"]," '} )
-#sed -i 's/<%=valor_disco>/'$USE_DISK'/g' $OUTPUT_FILE
+USE_DISK=$(df | tail -n+2 | awk {' print "['\''" $6 "'\'',"$3"],"'})
+sed -i 's/<%=valor_disco>/'$USE_DISK'/g' $OUTPUT_FILE
 
 #Memoria libre
 MEMORY_FREE=$(free -m | head -2 | tail -n 1 | awk ' {print $4 }')
